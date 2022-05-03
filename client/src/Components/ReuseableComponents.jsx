5b153242a5e582c741ed4e3c7cc709d862e41f80
Linkan Kumar Sahu <linkansahu2020@gmail.com>
2022-05-03 13:21:41 +0530

    r

--- client/src/Components/ReuseableComponents.jsx
+++ client/src/Components/ReuseableComponents.jsx
@@ -1,10 +1,7 @@
-import styled  from "styled-components";
+import React from 'react'
 
-export const Button = styled.button`
-border: 1px solid transparent;
-border-radius: 5vw;
-background: black;
-color: ${props=>props.color};
-padding: 1.5vh 2vw;
-font-weight: bold;
-`
+export default function ReuseableComponents() {
+  return (
+    <div>ReuseableComponents</div>
+  )
+}
